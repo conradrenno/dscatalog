f3bb9b0c98507b45641cbd821dde922f2ff06cc1
conradrenno <conradrenno@hotmail.com>
2025-08-20 13:57:49 -0300

    Adding test of controller get mapping methods

---
+++ backend/src/test/java/com/devrenno/dscatalog/controllers/ProductControllerTests.java
@@ -0,0 +1,86 @@
+package com.devrenno.dscatalog.controllers;
+
+import com.devrenno.dscatalog.dto.ProductDTO;
+import com.devrenno.dscatalog.services.ProductService;
+import com.devrenno.dscatalog.services.exceptions.ResourceNotFoundException;
+import com.devrenno.dscatalog.tests.Factory;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentMatchers;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+
+import java.util.List;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
+@WebMvcTest(ProductController.class)
+public class ProductControllerTests {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockitoBean
+    private ProductService service;
+
+    private ProductDTO productDTO;
+    private PageImpl<ProductDTO> page;
+
+    private long existingId;
+    private long nonExistingId;
+
+    @BeforeEach
+    void setUp() throws Exception {
+
+        existingId = 1L;
+        nonExistingId = 2L;
+
+        productDTO = Factory.createProductDTO();
+        page = new PageImpl<>(List.of(productDTO));
+
+        Mockito.when(service.findAll(ArgumentMatchers.any())).thenReturn(page);
+        Mockito.when(service.findById(existingId)).thenReturn(productDTO);
+        Mockito.when(service.findById(nonExistingId)).thenThrow(ResourceNotFoundException.class);
+    }
+
+    @Test
+    public void findAllShouldReturnPage() throws Exception {
+
+        ResultActions result =
+                mockMvc.perform(get("/products")
+                        .accept(MediaType.APPLICATION_JSON));
+
+        result.andExpect(status().isOk());
+    }
+
+    @Test
+    public void findByIdShouldReturnProductWhenIdExists() throws Exception {
+        ResultActions result =
+                mockMvc.perform(get("/products/{id}", existingId)
+                        .accept(MediaType.APPLICATION_JSON));
+
+        result.andExpect(status().isOk());
+        result.andExpect(jsonPath("$.id").exists());
+        result.andExpect(jsonPath("$.name").exists());
+        result.andExpect(jsonPath("$.description").exists());
+    }
+
+    @Test
+    public void findByIdShouldReturnNotFoundWhenIdDoesNotExists() throws Exception {
+        ResultActions result =
+                mockMvc.perform(get("/products/{id}", nonExistingId)
+                        .accept(MediaType.APPLICATION_JSON));
+
+        result.andExpect(status().isNotFound());
+    }
+
+}
