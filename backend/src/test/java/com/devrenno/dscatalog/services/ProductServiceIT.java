44be4d98e1041132540386fc657c0dd67e1c4d68
conradrenno <conradrenno@hotmail.com>
2025-08-22 15:28:16 -0300

    Integration testing for delete method

---
+++ backend/src/test/java/com/devrenno/dscatalog/services/ProductServiceIT.java
@@ -0,0 +1,46 @@
+package com.devrenno.dscatalog.services;
+
+import com.devrenno.dscatalog.repositories.ProductRepository;
+import com.devrenno.dscatalog.services.exceptions.ResourceNotFoundException;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ProductServiceIT {
+
+    @Autowired
+    private ProductService service;
+
+    @Autowired
+    private ProductRepository repository;
+
+    private Long existingId;
+    private Long nonExistingId;
+    private Long countTotalProducts;
+
+    @BeforeEach
+    void setUp() throws Exception {
+        existingId = 1L;
+        nonExistingId = 1000L;
+        countTotalProducts = 25L;
+    }
+
+    @Test
+    public void deleteShouldDeleteObjectWhenIdExists() {
+
+        service.delete(existingId);
+
+        Assertions.assertEquals(countTotalProducts - 1, repository.count());
+    }
+
+    @Test
+    public void deleteShouldThrowResourceNotFoundExceptionWhenIdExists() {
+
+        Assertions.assertThrows(ResourceNotFoundException.class, () -> {
+            service.delete(nonExistingId);
+        });
+    }
+}
