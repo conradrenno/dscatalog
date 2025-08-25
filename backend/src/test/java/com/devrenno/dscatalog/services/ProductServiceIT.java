1aba6b5d6fba8b9586dcdbcdc5155f09454dae1f
conradrenno <conradrenno@hotmail.com>
2025-08-25 11:24:05 -0300

    Adding integration testing for findAll method in service layer

--- backend/src/test/java/com/devrenno/dscatalog/services/ProductServiceIT.java
+++ backend/src/test/java/com/devrenno/dscatalog/services/ProductServiceIT.java
@@ -1,14 +1,21 @@
 package com.devrenno.dscatalog.services;
 
+import com.devrenno.dscatalog.dto.ProductDTO;
 import com.devrenno.dscatalog.repositories.ProductRepository;
 import com.devrenno.dscatalog.services.exceptions.ResourceNotFoundException;
+import jakarta.transaction.Transactional;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 
 @SpringBootTest
+@Transactional
 public class ProductServiceIT {
 
     @Autowired
@@ -42,5 +49,41 @@ public class ProductServiceIT {
         Assertions.assertThrows(ResourceNotFoundException.class, () -> {
             service.delete(nonExistingId);
         });
+    }
+
+    @Test
+    public void findAllShouldReturnPageWhenPage0Size10(){
+
+        Pageable pageable = PageRequest.of(0,10);
+
+        Page<ProductDTO> result = service.findAll(pageable);
+
+        Assertions.assertFalse(result.isEmpty());
+        Assertions.assertEquals(0,result.getNumber());
+        Assertions.assertEquals(10,result.getSize());
+        Assertions.assertEquals(countTotalProducts, result.getTotalElements());
+    }
+
+    @Test
+    public void findAllShouldReturnEmptyPageWhenPageDoesNotExists(){
+
+        Pageable pageable = PageRequest.of(50,10);
+
+        Page<ProductDTO> result = service.findAll(pageable);
+
+        Assertions.assertTrue(result.isEmpty());
+    }
+
+    @Test
+    public void findAllShouldReturnSortedPageWhenSortByName(){
+
+        Pageable pageable = PageRequest.of(0,10, Sort.by("name"));
+
+        Page<ProductDTO> result = service.findAll(pageable);
+
+        Assertions.assertFalse(result.isEmpty());
+        Assertions.assertEquals("Macbook Pro", result.getContent().get(0).getName());
+        Assertions.assertEquals("PC Gamer", result.getContent().get(1).getName());
+        Assertions.assertEquals("PC Gamer Alfa", result.getContent().get(2).getName());
     }
 }
