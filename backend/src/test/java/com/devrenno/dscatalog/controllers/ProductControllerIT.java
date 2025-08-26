ee9a42dbd0870d779061588aee8eb74a5ee15bc8
conradrenno <conradrenno@hotmail.com>
2025-08-25 21:57:34 -0300

    Adding integration testing for findAll and update methods in the controller layer

---
+++ backend/src/test/java/com/devrenno/dscatalog/controllers/ProductControllerIT.java
@@ -0,0 +1,96 @@
+package com.devrenno.dscatalog.controllers;
+
+import com.devrenno.dscatalog.dto.ProductDTO;
+import com.devrenno.dscatalog.services.ProductService;
+import com.devrenno.dscatalog.tests.Factory;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.transaction.Transactional;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@Transactional
+public class ProductControllerIT {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ProductService productService;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    private Long existingId;
+    private Long nonExistingId;
+    private Long countTotalProducts;
+    private ProductDTO productDTO;
+
+    @BeforeEach
+    void setUp() throws Exception {
+        existingId = 1L;
+        nonExistingId = 1000L;
+        countTotalProducts = 25L;
+        productDTO = Factory.createProductDTO();
+    }
+
+    @Test
+    public void findAllShouldReturnSortedPageWhenSortByName() throws Exception {
+
+        ResultActions result =
+                mockMvc.perform(MockMvcRequestBuilders.get("/products?page=0&size=10&sort=name,asc")
+                        .accept(MediaType.APPLICATION_JSON));
+        result.andExpect(status().isOk());
+        result.andExpect(jsonPath("$.totalElements").value(countTotalProducts));
+        result.andExpect(jsonPath("$.content").exists());
+        result.andExpect(jsonPath("$.content[0].name").value("Macbook Pro"));
+        result.andExpect(jsonPath("$.content[1].name").value("PC Gamer"));
+        result.andExpect(jsonPath("$.content[2].name").value("PC Gamer Alfa"));
+    }
+
+    @Test
+    public void updateShouldReturnProductDTOWhenIdExists() throws Exception {
+
+        String jsonBody = objectMapper.writeValueAsString(productDTO);
+
+        String expectedName = productDTO.getName();
+        String expectedDescription = productDTO.getDescription();
+
+        ResultActions result =
+                mockMvc.perform(put("/products/{id}", existingId)
+                        .content(jsonBody)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON));
+
+        result.andExpect(status().isOk());
+        result.andExpect(jsonPath("$.id").exists());
+        result.andExpect(jsonPath("$.name").value(expectedName));
+        result.andExpect(jsonPath("$.description").value(expectedDescription));
+    }
+    @Test
+    public void updateShouldReturnNotFoundWhenIdDoesNotExist() throws Exception {
+
+        String jsonBody = objectMapper.writeValueAsString(productDTO);
+
+        ResultActions result =
+                mockMvc.perform(put("/products/{id}", nonExistingId)
+                        .content(jsonBody)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON));
+
+        result.andExpect(status().isNotFound());
+    }
+
+}
